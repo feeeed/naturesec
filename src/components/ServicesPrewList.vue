789684d1e225d4cd4892dfc7848db0b806cd66cf
feeeed <mak-eg@mail.ru>
2024-07-23 19:10:33 +0600

    new lists

---
+++ src/components/ServicesPrewList.vue
@@ -0,0 +1,33 @@
+<template>
+    <p class="text-body-1 font-weight-medium text-medium-emphasis mb-10">
+        Похожее:
+      </p>
+  <v-row> 
+   <service-card
+   v-for="(service,key) in servicesEk"
+   :key="key"
+   :id="service.id"
+   :title="service.title"
+   :image="service.image"
+   />
+  </v-row>
+</template>
+
+<script>
+import ServiceCard from "./layouts/ServiceCard.vue";
+export default {
+  name: "service-card-list",
+  components:{
+    ServiceCard,
+
+  },
+  props: {
+    servicesEk: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
